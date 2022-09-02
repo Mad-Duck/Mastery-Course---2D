b157b0c85e6060eda6cd76b11189ac7d408b0b9b
L_Mac <7247231+Mad-Duck@users.noreply.github.com>
2022-09-02 10:34:35 -0700

    Added vectors to PlayerMovementController script

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, vertical);
+
+        Debug.Log(movement);
     }
 }
